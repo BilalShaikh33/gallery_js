0086e772ef81f0bae7e6aca78d6dc2f1003260fb
rimshakh027-png <rimshakh027@gmail.com>
2025-11-23 16:37:07 +0500

    i

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var gallery = [
-    "https://upload.wikimedia.org/wikipedia/commons/2/23/US_one_dollar_bill%2C_obverse%2C_series_2009.jpg",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNPBJeCI5Zn0H6bVe9TwlEr8bBTQe5f42DGQ&s",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdYlECKGgTE1fNTMa_tF6Upnp4O7EKv8C01g&s",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmfDg6lWT_71HaElHxKu11quu1Y7dNkH9y6w&s",
+    "https://www.psu.com/wp/wp-content/uploads/2021/08/ghost-of-tsushima-directors-cut-review-ps5-4.jpg",
+    "https://cdn.wccftech.com/wp-content/uploads/2018/07/ghost_tsushima_battlefield-scaled.jpg",
+    "https://i.redd.it/iyiug45hoccc1.jpeg",
+    "https://www.psu.com/wp/wp-content/uploads/2021/08/ghost-of-tsushima-directors-cut-review-ps5-1.jpg",
 
 
 ]

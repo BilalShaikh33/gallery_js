37ceff7aa184f0aab35c317217b97aefc4b8be1a
Bilal Shaikh <bilalshaikhh339@gmail.com>
2025-11-25 16:54:44 +0500

    i

--- app.js
+++ app.js
@@ -1,36 +1,36 @@
-var gallery = [
-    "./images/img4.jpg",
-    "./images/img3.jpg",
-    "./images/img2.jpg",
-    "./images/img1.jpg" ,
+// var gallery = [
+//     "./images/img4.jpg",
+//     "./images/img3.jpg",
+//     "./images/img2.jpg",
+//     "./images/img1.jpg" ,
 
 
-]
-var imgShow = document.getElementById("imgShow")
-var disabbut = document.getElementById("nexbut")
-var prevbut = document.getElementById("prevbut")
-var nextbut = document.getElementById("nexbut")
-var currentIndex = 0 
+// ]
+// var imgShow = document.getElementById("imgShow")
+// var disabbut = document.getElementById("nexbut")
+// var prevbut = document.getElementById("prevbut")
+// var nextbut = document.getElementById("nexbut")
+// var currentIndex = 0 
 
-function nextImg(){
-    currentIndex +=1
-    imgShow.src = gallery[currentIndex]
-    if (currentIndex ==  3) {   
-        disabbut.style.display = "none" 
-    }
-    if (currentIndex == 1) {  
-        prevbut.style.display = "inline"      
-    }
-}
-function prevImg(){
-    currentIndex -=1
-    imgShow.src = gallery[currentIndex]
+// function nextImg(){
+//     currentIndex +=1
+//     imgShow.src = gallery[currentIndex]
+//     if (currentIndex ==  3) {   
+//         disabbut.style.display = "none" 
+//     }
+//     if (currentIndex == 1) {  
+//         prevbut.style.display = "inline"      
+//     }
+// }
+// function prevImg(){
+//     currentIndex -=1
+//     imgShow.src = gallery[currentIndex]
 
-    if (currentIndex ==  2 ) {  
-        nextbut.style.display = "inline"
-    }
+//     if (currentIndex ==  2 ) {  
+//         nextbut.style.display = "inline"
+//     }
 
-    if (currentIndex == 0) {
-        prevbut.style.display = "none"
-    }
-}
+//     if (currentIndex == 0) {
+//         prevbut.style.display = "none"
+//     }
+// }

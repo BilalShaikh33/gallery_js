728a9537e80c8041771c5fed66b8174a60a91799
Bilal Shaikh <bilalshaikhh339@gmail.com>
2025-11-25 16:54:24 +0500

    i

--- app.js
+++ app.js
@@ -1,43 +1,27 @@
 var gallery = [
-    "https://www.psu.com/wp/wp-content/uploads/2021/08/ghost-of-tsushima-directors-cut-review-ps5-4.jpg",
-    "https://cdn.wccftech.com/wp-content/uploads/2018/07/ghost_tsushima_battlefield-scaled.jpg",
-    "https://i.redd.it/iyiug45hoccc1.jpeg",
-    "https://www.psu.com/wp/wp-content/uploads/2021/08/ghost-of-tsushima-directors-cut-review-ps5-1.jpg",
+    "./images/img4.jpg",
+    "./images/img3.jpg",
+    "./images/img2.jpg",
+    "./images/img1.jpg" ,
 
 
 ]
-
 var imgShow = document.getElementById("imgShow")
 var disabbut = document.getElementById("nexbut")
 var prevbut = document.getElementById("prevbut")
 var nextbut = document.getElementById("nexbut")
-
 var currentIndex = 0 
-
 
 function nextImg(){
     currentIndex +=1
     imgShow.src = gallery[currentIndex]
-
-    if (currentIndex ==  3) {
-        
-        disabbut.style.display = "none"
-
-   
-        
-        
+    if (currentIndex ==  3) {   
+        disabbut.style.display = "none" 
     }
-
-    if (currentIndex == 1) {
-            
-        prevbut.style.display = "inline"
-        
+    if (currentIndex == 1) {  
+        prevbut.style.display = "inline"      
     }
 }
-
-
-
-
 function prevImg(){
     currentIndex -=1
     imgShow.src = gallery[currentIndex]
